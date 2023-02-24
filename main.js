77fcfba63c9aa38c8161fd3d2d3740b97bdf1f85
rockeven199 <67353571+rockeven199@users.noreply.github.com>
2023-02-24 20:14:51 +0800

    submit

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
-const { BrowserWindow, app } = require("electron");
-const path = require("path")
+const { BrowserWindow, app, Tray, ipcMain ,Menu} = require("electron");
+const path = require("path");
+const menuTemplate=require("./js/menu");
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 300,
     height: 200,
+    // height:600,
     x: 1200,
     y: 100,
     resizable: false,
     frame: false,
-    backgroundColor: 'rgba(0, 0, 0, 0)',
-    skipTaskbar:true,
-    alwaysOnTop:true
+    backgroundColor: 'rgba(0,0,0,0)',
+    icon: path.resolve(__dirname, "logo.ico"),
+    webPreferences: {
+      preload: path.resolve(__dirname, "./js/preload.js")
+    }
   });
   mainWindow.loadFile(path.resolve(__dirname, "index.html"));
-  mainWindow.webContents.openDevTools();
+  mainWindow.setAlwaysOnTop(false, "normal")
+  // mainWindow.webContents.openDevTools();
 }
 
 app.whenReady().then(() => {
